d8c8e4af954492cb8984125f22b3ffd8b52a209f
Rafael Neri <rafepel@gmail.com>
2022-06-17 22:19:34 -0300

    Update menu helper

--- src/helpers/menu_helper.ts
+++ src/helpers/menu_helper.ts
@@ -1,3 +1,27 @@
-/* export const checkVisibleMenu = (links: any) => {
-  // console.log(links.teste);
-}; */
+/* eslint-disable dot-notation */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
+import { RouteRecordRaw } from 'vue-router';
+
+export const proccessMenu = (routes: RouteRecordRaw[]): RouteRecordRaw[] => {
+  // Add dynamic routes
+  // routes[0].children = [];
+
+  // links['main'].forEach((item) => {
+  //   if (item.status === Status.ENABLE) {
+  //     const route = <RouteRecordRaw>{
+  //       name: item.name,
+  //       path: item.path,
+  //     };
+  //     if (item.component !== undefined) {
+  //       route.component = item.component;
+  //     }
+  //     routes[0].children?.push(route);
+  //   }
+  // });
+
+  console.log(routes);
+
+  return routes;
+};
